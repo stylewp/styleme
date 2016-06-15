dde0e8e3f709c0716730f49adf566f6cafef1856
Hajduk Split <Hajduk Split>
2016-06-15 10:57:34 +0200

    bootstrap override

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -6,7 +6,6 @@
  *
  * @package styleme
  */
-
 if ( ! function_exists( 'styleme_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
@@ -16,29 +15,36 @@ function styleme_posted_on() {
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
-
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_attr( get_the_modified_date( 'c' ) ),
 		esc_html( get_the_modified_date() )
 	);
-
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'styleme' ),
+		esc_html_x( ' %s', 'post date', 'styleme' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
-
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'styleme' ),
+		esc_html_x( ' %s', 'post author', 'styleme' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
+	echo '<span class="posted-on"><i class="fa fa-calendar" aria-hidden="true"></i>' . $posted_on . '</span><span class="byline"><i class="fa fa-user" aria-hidden="true"></i> ' . $byline . '</span>'; // WPCS: XSS OK.
+  if ( 'post' === get_post_type() ) {
+    /* translators: used between list items, there is a space after the comma */
+    $categories_list = get_the_category_list( esc_html__( ', ', 'styleme' ) );
+    if ( $categories_list && styleme_categorized_blog() ) {
+      printf( '<span class="cat-links"><i class="fa fa-folder-open" aria-hidden="true"></i> ' . esc_html__( ' %1$s', 'styleme' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+    }
+  }
+  if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+    echo '<span class="comments-link"><i class="fa fa-comments" aria-hidden="true"></i> ';
+    /* translators: %s: post title */
+    comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'styleme' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+    echo '</span>';
+  }
 }
 endif;
-
 if ( ! function_exists( 'styleme_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.
@@ -47,37 +53,22 @@ function styleme_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'styleme' ) );
-		if ( $categories_list && styleme_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'styleme' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
-
-		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'styleme' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'styleme' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			printf( '<span class="tags-links"><i class="fa fa-tags" aria-hidden="true"></i> ' . esc_html__( ' %1$s', 'styleme' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		/* translators: %s: post title */
-		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'styleme' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
-		echo '</span>';
-	}
-
 	edit_post_link(
 		sprintf(
 			/* translators: %s: Name of current post */
 			esc_html__( 'Edit %s', 'styleme' ),
 			the_title( '<span class="screen-reader-text">"', '"</span>', false )
 		),
-		'<span class="edit-link">',
+		'<span class="edit-link alignright">',
 		'</span>'
 	);
 }
 endif;
-
 /**
  * Returns true if a blog has more than 1 category.
  *
@@ -92,13 +83,10 @@ function styleme_categorized_blog() {
 			// We only need to know if there is more than one category.
 			'number'     => 2,
 		) );
-
 		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
-
 		set_transient( 'styleme_categories', $all_the_cool_cats );
 	}
-
 	if ( $all_the_cool_cats > 1 ) {
 		// This blog has more than 1 category so styleme_categorized_blog should return true.
 		return true;
@@ -107,7 +95,6 @@ function styleme_categorized_blog() {
 		return false;
 	}
 }
-
 /**
  * Flush out the transients used in styleme_categorized_blog.
  */
