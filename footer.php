fc95a3eb6f4bea68cbc2db8af541866d726e484f
Bojan Z <stylewpnet@gmail.com>
2016-06-15 14:58:41 +0200

    footer style and post navigation

--- footer.php
+++ footer.php
@@ -15,11 +15,13 @@
   </div><!-- container -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+	  <div class="container">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'styleme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'styleme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'styleme' ), 'styleme', '<a href="http://stylewp.net/" rel="designer">StyleWp</a>' ); ?>
 		</div><!-- .site-info -->
+	   </div><!-- .container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
