aa8a1913bcca1c022ee9b52314e213eec4238f25
Bojan Z <stylewpnet@gmail.com>
2016-06-15 15:06:12 +0200

    cleanup

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ StyleMe - Bootstrap Wordpress Starter Theme
 Getting Started
 ---------------
 
+
+Final Step
+----------
+
+When you theme is 100% done navigate to /inc/scripts.php and change the call to your stylesheet to use the minified version that Gulp create with the following code:
+wp_enqueue_style( 'strapped-style', get_template_directory_uri() . '/style.min.css' );
--- inc/scripts.php
+++ inc/scripts.php
@@ -7,8 +7,6 @@ function styleme_scripts() {
 
 	wp_enqueue_script( 'styleme-navigation', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), '1.0.0', true );
 
-	wp_enqueue_script( 'styleme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
